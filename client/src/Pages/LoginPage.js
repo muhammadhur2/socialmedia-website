c71a480347a7ff5e1580a8c151fdea7f87777646
zaidharis2801 <s.haris.22868@khi.iba.pk>
2023-11-01 20:49:31 +0500

    created pages

---
+++ client/src/Pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginSignup from '../Components/LoginSignup/LoginSignup'; // Adjust the path if needed
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ client/src/Pages/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from '../Components/Profile/Profile';  // Adjust the path if needed
+
+const ProfilePage = () => {
+  return (
+    <div>
+      
+      <Profile />
+    </div>
+  );
+};
+
+export default ProfilePage;
