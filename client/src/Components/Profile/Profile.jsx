67999aaceb326836e07fb296c2c060b4420e1347
zaidharis2801 <s.haris.22868@khi.iba.pk>
2023-11-01 21:34:55 +0500

    added edit profile

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -3,42 +3,75 @@ import userService from '../../Services/UserService';
 import UserContext from '../../UserContext';
 
 const Profile = () => {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const [profileData, setProfileData] = useState(null);
-
-  console.log("User Context:", user); // To check the value from the context
+  const [isEditing, setIsEditing] = useState(false);
+  const [editData, setEditData] = useState({ name: '', email: '' });
 
   useEffect(() => {
     const fetchProfile = async () => {
-      // Safety check to see if user and user.token exist
       if (user && user.token) {
-        console.log("Attempting to fetch profile with token:", user.token);
         try {
           const response = await userService.getProfile(user.token);
-          console.log("API Response:", response.data); // Log the API response
-
           if (response.data.status === 'ok') {
             setProfileData(response.data.user);
-          } else {
-            console.error("Error fetching profile", response.data.error);
+            setEditData({ name: response.data.user.name, email: response.data.user.email });
           }
         } catch (error) {
           console.error("Error fetching profile", error);
         }
-      } else {
-        console.warn("User or user token missing."); // Added this for clarity
       }
     };
-    
     fetchProfile();
   }, [user]);
 
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditData({ ...editData, [name]: value });
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await userService.updateProfile(editData, user.token);
+      if (response.data.status === 'ok') {
+        setProfileData(response.data.user);
+        setUser({ ...user, ...response.data.user });
+        setIsEditing(false);
+      }
+    } catch (error) {
+      console.error("Error updating profile", error);
+    }
+  };
+
   return (
     <div>
-      {profileData ? (
+      {isEditing ? (
+        <>
+          <h2>Edit Profile</h2>
+          <input
+            type="text"
+            name="name"
+            value={editData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            value={editData.email}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleUpdate}>Save Changes</button>
+          <button onClick={handleEdit}>Cancel</button>
+        </>
+      ) : profileData ? (
         <>
           <h2>{profileData.name}'s Profile</h2>
           <p>Email: {profileData.email}</p>
+          <button onClick={handleEdit}>Edit Profile</button>
         </>
       ) : (
         <p>Loading...</p>
