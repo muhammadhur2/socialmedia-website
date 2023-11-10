113fddd3d9373cb6bd13db0c29519027e3a18aa4
CommanderAta <commanderata@gmail.com>
2023-11-10 21:38:47 +0500

    Musab boldia mujhe ajeeb

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginSignup from '../Components/LoginSignup_Cmmndr/LoginSignup'; // Adjust the path if needed
+import LoginSignup from '../Components/LoginSignup_Material/LoginSignup'; // Adjust the path if needed
 
 const LoginPage = () => {
   return (
