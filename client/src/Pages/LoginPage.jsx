5bff1f37e2a6671560e16e196a9233ae8e3d5045
CommanderAta <commanderata@gmail.com>
2023-11-10 20:37:26 +0500

    Login/Signup sexied

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginSignup from '../Components/LoginSignup/LoginSignup'; // Adjust the path if needed
+import LoginSignup from '../Components/LoginSignup_by_Atta/LoginSignup'; // Adjust the path if needed
 
 const LoginPage = () => {
   return (
