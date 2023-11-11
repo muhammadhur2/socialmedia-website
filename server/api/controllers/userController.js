df060a532f43501f9e0a8c1176f24be4dfbc9943
CommanderAta <commanderata@gmail.com>
2023-11-11 12:09:48 +0500

    Some validations

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -2,52 +2,58 @@ const User = require('../models/user.model');
 const bcrypt = require('../utils/bcryptHelper.js');
 const jwt = require('../utils/jwtHelper');
 const { verifyToken } = require('../utils/jwtHelper');
+const { body, validationResult } = require('express-validator');
 
-exports.register = async (req, res) => {
-    try {
-      console.log(req.body);
-        if (!req.body.password) {
-            return res.json({ status: 'error', error: 'Password is null' });
-        }
-        const hashedPassword = await bcrypt.hashPassword(req.body.password);
-        
-        // Debug log to check what is actually being saved
-                // Debug log to check what is actually being saved
+exports.register = [
+  body('name').trim().isLength({ min: 1 }).withMessage('Name is required'),
+  body('email').isEmail().withMessage('Invalid email format'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
 
-        console.log('Hashed Password:', hashedPassword);
-        
-        await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.json({ status: 'ok' });
-    } catch (err) {
-        console.log(err);
-        res.json({ status: 'error', error: 'Error' });
-    }
-};
-
-exports.login = async (req, res) => {
-  try {
-      console.log(req.body);
-
-      const user = await User.findOne({ email: req.body.email });
-      if (user) {
-          const match = await bcrypt.comparePassword(req.body.password, user.password);
-          if (match) {
-              const token = jwt.generateToken(user);
-              return res.json({ status: 'ok', token });
-          } else {
-              return res.json({ status: 'error', message: 'Incorrect password' });
-          }
-      } else {
-          return res.json({ status: 'error', message: 'User not found' });
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
       }
-  } catch (err) {
-      res.json({ status: 'error', message: 'Internal server error' });
+
+      try {
+          const hashedPassword = await bcrypt.hashPassword(req.body.password);
+          await User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: hashedPassword
+          });
+          res.json({ status: 'ok' });
+      } catch (err) {
+          console.log(err);
+          res.json({ status: 'error', error: 'Error' });
+      }
   }
-};
+];
+
+exports.login = [
+  body('email').isEmail().withMessage('Invalid email format'),
+  body('password').exists().withMessage('Password is required'),
+
+  async (req, res) => {
+      try {
+          const user = await User.findOne({ email: req.body.email });
+          if (user) {
+              const match = await bcrypt.comparePassword(req.body.password, user.password);
+              if (match) {
+                  const token = jwt.generateToken(user);
+                  return res.json({ status: 'ok', token });
+              } else {
+                  return res.json({ status: 'error', message: 'Incorrect password' });
+              }
+          } else {
+              return res.json({ status: 'error', message: 'User not found' });
+          }
+      } catch (err) {
+          res.json({ status: 'error', message: 'Internal server error' });
+      }
+  }
+];
+
 
 
 
@@ -65,29 +71,34 @@ exports.getProfile = async (req, res) => {
     }
 };
 
-exports.updateProfile = async (req, res) => {
-    try {
-        console.log("Request Body: ", req.body);  // Debug Line
+exports.updateProfile = [
+  body('name').optional().trim().isLength({ min: 1 }).withMessage('Name is required'),
+  body('email').optional().isEmail().withMessage('Invalid email format'),
 
-        const updatedUser = await User.findOneAndUpdate(
-            { email: req.user.email },
-            { $set: { name: req.body.name, email: req.body.email } },
-            { new: true, fields: { password: 0 }, w: "majority" }
-        );
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
 
-        console.log("Updated User: ", updatedUser);  // Debug Line
+      try {
+          const updatedUser = await User.findOneAndUpdate(
+              { email: req.user.email },
+              { $set: { name: req.body.name, email: req.body.email } },
+              { new: true, fields: { password: 0 }, w: "majority" }
+          );
 
-        if (updatedUser) {
-            return res.json({ status: 'ok', user: updatedUser });
-        } else {
-            return res.json({ status: 'error', error: 'User not found' });
-        }
-    } catch (err) {
-        console.log("Error in Update: ", err);  // Debug Line
-        return res.json({ status: 'error', error: 'Update failed' });
-    }
-};
-
+          if (updatedUser) {
+              return res.json({ status: 'ok', user: updatedUser });
+          } else {
+              return res.json({ status: 'error', error: 'User not found' });
+          }
+      } catch (err) {
+          console.log("Error in Update: ", err);
+          return res.json({ status: 'error', error: 'Update failed' });
+      }
+  }
+];
 exports.deleteAccount = async (req, res) => {
     try {
       // Extract user ID from verified JWT token
