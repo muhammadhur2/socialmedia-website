ea1f4e260631685b8beec19cdec03d41b7e79fd9
CommanderAta <commanderata@gmail.com>
2023-11-20 15:11:58 +0500

    Friends fr fr

--- client/src/Pages/FriendsPage.jsx
+++ client/src/Pages/FriendsPage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 // import AppBar from '../Components/AppBar/AppBar';  // Adjust the path if needed
 import AppBar_material from '../Components/AppBar_Material/AppBar';  // Adjust the path if needed
 // import Profile from '../Components/Profile_Material/Profile';  // Adjust the path if needed
+import FriendsPage from '../Components/Friends/Friends';
 
 const ProfilePage = () => {
   return (
     <div>
       <AppBar_material />  {/* Adding the AppBar here */}
-      {/* <Profile /> */}
+      <FriendsPage />
     </div>
   );
 };
