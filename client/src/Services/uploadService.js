8ebcbfd75fe17fcdcade9ff10df9bad8c4e9b1f3
Muhammad Hur <muhammedhur@gmail.com>
2023-12-13 05:37:56 +0500

    route link fixed

--- client/src/Services/uploadService.js
+++ client/src/Services/uploadService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://socialmedia-website-three.vercel.app/";  // Replace with your actual API URL
+const API_URL = "https://socialmedia-website-three.vercel.app";  // Replace with your actual API URL
 // const API_URL = "http://localhost:3001";  // Replace with your API URL
 
 class uploadService {
