ed69d62fdc277d2fd0facfd523f3e441eb823806
CommanderAta <commanderata@gmail.com>
2023-12-27 17:16:16 +0500

    Right side bar pe friends dikh rahe yayyy

--- client/src/Components/RightSideBar/RightSideBar.jsx
+++ client/src/Components/RightSideBar/RightSideBar.jsx
@@ -1,59 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
 import { useNavigate } from 'react-router-dom';
+import UserContext from '../../UserContext'; // Import UserContext
+import UserService from '../../Services/UserService'; // Import UserService
 
 const drawerWidth = 240;
 
 export default function RightPermanentDrawer() {
   const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const [friends, setFriends] = useState([]);
 
-  const handleNavigation = (path) => {
-    navigate(path);
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        if (user?.token) {
+          const response = await UserService.listFriends(user.token);
+          setFriends(response.data.friends);
+        }
+      } catch (error) {
+        console.error('Error fetching friends:', error);
+      }
+    };
+
+    fetchFriends();
+  }, [user]);
+
+  const handleNavigation = (friendId) => {
+    navigate(`/friend/${friendId}`);
   };
-
-  const list = () => (
-    <Box
-      sx={{ width: drawerWidth }}
-      role="presentation"
-    >
-      <List>
-        {[' ', ' ',' ','Feed', 'Friends', 'Create Challenge'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={() => handleNavigation('/' + text.toLowerCase().replace(/\s+/g, ''))}>
-              <ListItemIcon>
-                {text === 'Friends' && <PeopleAltOutlinedIcon />}
-                {text === 'Create Challenge' && <FitnessCenterOutlinedIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </Box>
-  );
 
   return (
     <Drawer
       variant="permanent"
       open
-      anchor="right" // Positioning drawer to the right
+      anchor="right"
       sx={{
         width: drawerWidth,
         flexShrink: 0,
         [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
       }}
     >
-      {list()}
+      <Box sx={{ width: drawerWidth, pt: 10 }} role="presentation"> {/* Added padding-top here */}
+        <List>
+          {friends.map((friend) => (
+            <ListItem key={friend._id} disablePadding>
+              <ListItemButton onClick={() => handleNavigation(friend._id)}>
+                <ListItemAvatar>
+                  <Avatar src={friend.profilePicture || '/path/to/default/avatar.jpg'} />
+                </ListItemAvatar>
+                <ListItemText primary={friend.name} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Box>
     </Drawer>
   );
 }
